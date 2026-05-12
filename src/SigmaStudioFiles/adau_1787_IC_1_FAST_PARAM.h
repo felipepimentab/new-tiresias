008e82f9ab9a082900e10976b4b0922e8350ec00
felipepimentab <felipe.pbernardo@gmail.com>
2026-05-12 16:40:18 -0300

    add sigma studio files

---
+++ src/SigmaStudioFiles/adau_1787_IC_1_FAST_PARAM.h
@@ -0,0 +1,22 @@
+/*
+ * File:           C:\users\crossover\Desktop\exports\adau_1787_IC_1_FAST_PARAM.h
+ *
+ * Created:        Monday, May 11, 2026 4:05:20 PM
+ * Description:    adau_1787:IC 1-Fast parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2026 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __ADAU_1787_IC_1_FAST_PARAM_H__
+#define __ADAU_1787_IC_1_FAST_PARAM_H__
+
+#endif
---
+++ src/SigmaStudioFiles/defines.h
@@ -0,0 +1,29 @@
+/*
+ * File:           defines.h
+ *
+ * Created:        Monday, May 11, 2026 4:05:20 PM
+ * Description:    adau_1787 IC default download data definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2026 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#define BufferSize_IC_1_SIGMA     	1102
+#define NumTransactions_IC_1_SIGMA	210
+
+#define BufferSize_IC_1_FAST     	6
+#define NumTransactions_IC_1_FAST	2
+
+
+#endif
