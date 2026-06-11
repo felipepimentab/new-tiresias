f30c521305717f5916588e734f4fb0d07ee5516e
felipepimentab <felipe.pbernardo@gmail.com>
2026-06-11 12:12:18 -0300

    Update adau program with stereo switch between asrc and passthrough

--- src/SigmaStudioFiles/defines.h
+++ src/SigmaStudioFiles/defines.h
@@ -1,7 +1,7 @@
 /*
  * File:           defines.h
  *
- * Created:        Monday, May 18, 2026 11:16:45 PM
+ * Created:        Wednesday, June 10, 2026 3:09:05 PM
  * Description:    adau_1787 IC default download data definitions.
  *
  * This software is distributed in the hope that it will be useful,
@@ -19,8 +19,8 @@
 #ifndef __DEFINES_H__
 #define __DEFINES_H__
 
-#define BufferSize_IC_1_SIGMA     	1037
-#define NumTransactions_IC_1_SIGMA	210
+#define BufferSize_IC_1_SIGMA     	1366
+#define NumTransactions_IC_1_SIGMA	209
 
 #define BufferSize_IC_1_FAST     	6
 #define NumTransactions_IC_1_FAST	2
